b038a71789cb3fcbfdc10db02eb053032f241476
Makasevic <diego.ufrj@yahoo.com.br>
2024-12-31 09:04:47 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+NOTION_TOKEN = "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"
+DATABASE_ID = "165d12cbe28e80eb9f7ad9d83cdd7115"
+
+HEADERS = {
+    "Authorization": f"Bearer {NOTION_TOKEN}",
+    "Content-Type": "application/json",
+    "Notion-Version": "2022-06-28",
+}
